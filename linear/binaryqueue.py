146fb3acb39d9e0cfd24a315472e7d34423dbb9f
Pasindu Jayalal <PD294@canterbury.ac.uk>
2025-10-02 23:58:27 +0100

    Binay numbers in queue

---
+++ linear/binaryqueue.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+
+class BinaryQueue:
+    def __init__(self):
+        self._queue = deque()
+        
+    def enqueue(self, item):
+        self._queue.appendleft(item)
+        
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("dequeue from an empty queue")
+        return self._queue.pop()
+    
+    def is_empty(self) -> bool:
+        return len(self._queue) == 0
+    
+    def size(self) -> int:
+        return len(self._queue)
+    
+    def print(self):
+        if self.is_empty():
+            print("Queue is empty")
+            return
+        print(self._queue)
+        
+    def front(self):
+        if self.is_empty():
+            raise IndexError("front from an empty queue")
+        return self._queue[-1]
+    
+
+def binary_count(n):
+    bq = BinaryQueue()
+    for i in range(n):
+        s = bq.front()
+        bq.enqueue(n)
+        print(s) 
+        
+        bq.enqueue(s + "0")
+        bq.enqueue(s + "1")
+
+
+if __name__ == "__main__":
+    num = 10
+    binary_count(num)
+    
+       
+        
